9821aed7cd1e33eb93b3a56b6cab6c8239fc24b1
TarekBarodi <Tarek.barodi@gmail.com>
2019-10-22 10:42:24 +0200

    add getSortedSwimmers, writeSwimmerCSV and to be work on printSwimmersThroughServer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -8,18 +13,99 @@ import java.util.stream.Collectors;
 
 public class Main
 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
         List <Animal> animals;
         animals = readAnimalFromCSV();
 
-        // sorting
+        // sorting the animals alphabetically by name
         List<Animal> sortedAnimals = animals.stream().sorted(Comparator.comparing(Animal::getName)).collect(Collectors.toList());
 
+        // print the sorted animal list
         printListOfAnimals(sortedAnimals);
 
+        // the below code is the one used to create the method easily.
         //List<Animal> filterAnimalList = filteredAnimalList (animals, FilterOptions.TYPE, "shark");
 
+        //filtering swimmer objects from animals list, and casting to Swimmer type and collect again to list.
+        List<Swimmer> swimmers = animals.stream().filter(a -> a instanceof Swimmer).map(x -> (Swimmer) x).collect(Collectors.toList());
+
+        List<Swimmer> sortedSwimmers = getSortedSwimmers(swimmers);
+
+        /**
+        writeSwimmerCSV(sortedSwimmers);
+
+        printSwimmersThroughServer(sortedSwimmers);
+         */
+
     }
+
+
+    private static List<Swimmer> getSortedSwimmers(List<Swimmer> swimmers)
+    {
+        //Swimmer interface does not have a birth date field, therefore we filter out the objects that are of type
+        //Animal
+        List<Animal> animalSwimmers = swimmers.stream().map(x -> (Animal) x).collect(Collectors.toList());
+
+        //Sorting the Animal list that are of Swimmer type chronologically by birth date and
+        //return a list of Swimmer type
+        List<Swimmer> sortedSwimmersByBirthYear = animalSwimmers.stream()
+                .sorted(Comparator.comparing(Animal::getBirthYear)).map(x -> (Swimmer)x)
+                .collect(Collectors.toList());
+
+        return sortedSwimmersByBirthYear;
+    }
+
+    /**
+     *
+    private static void printSwimmersThroughServer(List<Swimmer> swimmers)
+    {
+        //Convert the list of swimmers to a string
+        String swimmersString = "";
+
+        for (Swimmer swimmer: swimmers)
+        {
+            swimmersString = swimmersString + (swimmer.getClass().cast(swimmer)).toString();
+        }
+
+        //Try connect to the server on an unused port eg 9991. A successful connection will return a socket
+        try(ServerSocket serverSocket = new ServerSocket(9991))
+        {
+            Socket connectionSocket = serverSocket.accept();
+
+            //Create Output stream for the connection
+            InputStream inputFromServer = connectionSocket.getInputStream();
+            OutputStream serverPrintOut = connectionSocket.getOutputStream();
+
+            Scanner scanner = new Scanner(swimmersString);
+            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
+
+            while (scanner.hasNextLine())
+            {
+                String line = scanner.nextLine();
+                serverPrintOut.println((line));
+            }
+
+            System.out.println(inputFromServer.readAllBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+*/
+
+
+    public static void writeSwimmerCSV(List<Swimmer> swimmers) throws IOException {
+        //Convert the list of swimmers to a string
+        String swimmersString = "";
+        for (Swimmer swimmer: swimmers) {
+            swimmersString = swimmersString + (swimmer.getClass().cast(swimmer)).toString();
+        }
+        Path path = Paths.get("/Users/tarekbarodi/Desktop/AnimalsApp/Animals.csv");
+        Files.writeString(path,swimmersString);
+    }
+
+
 
     /**
      * This method return the filtered list by type, name or year of birth
@@ -141,7 +227,7 @@ public class Main
                 animal.setName(name);
                 animal.setBirthYear(Integer.parseInt(strYear));
 
-                // adding the animal object to the list before proceding to the next line
+                // adding the animal object to the list before preceding to the next line
                 list.add(animal);
             }
                 else
